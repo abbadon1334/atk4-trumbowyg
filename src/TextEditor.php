34c597e19b9cee7eb75af660c4eaffd1a2e38be8
abbadon1334 <fdanti@gmail.com>
2019-08-29 20:53:23 +0300

    first commit

---
+++ src/TextEditor.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace atk4\ui\FormField;
+
+class TextEditor extends TextArea
+{
+    public $defaultTemplate = __DIR__ . '/../template/trumbowyg.html';
+
+    private static $loaded_assets = [];
+
+    public $assets_path = '/assets';
+
+    protected $required_js = [
+        '/trumbowyg.js',
+    ];
+
+    protected $required_css = [
+        '/ui/trumbowyg.css',
+    ];
+
+    public $option_resetCss = true;
+    public $option_autogrow = true;
+
+    public $editor_options = [
+        'btns' => [
+            ['viewHTML'],
+            ['undo', 'redo'], // Only supported in Blink browsers
+            ['formatting'],
+            ['strong', 'em', 'del'],
+            ['superscript', 'subscript'],
+            ['link'],
+            ['insertImage'],
+            ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+            ['unorderedList', 'orderedList'],
+            ['horizontalRule'],
+            ['removeformat'],
+            ['fullscreen']
+        ],
+        'resetCss' => true,
+        'autogrow' => true
+    ];
+
+    public $plugins = [];
+
+    public function init()
+    {
+        parent::init();
+
+        $this->addRequiredAssets();
+        foreach($this->plugins as $plugin)
+        {
+
+        }
+        //$this->setStyle('display','block');
+
+        $this->editor_options['resetCss'] = $this->option_resetCss;
+        $this->editor_options['autogrow'] = $this->option_autogrow;
+
+        $this->jsInput(true)->trumbowyg($this->editor_options);
+    }
+
+    private function addRequiredAssets() :void
+    {
+        foreach ($this->required_js as $js) {
+
+            if($this->isAssetLoaded($js)) {
+                continue;
+            }
+
+            self::$loaded_assets[] = $js;
+
+            $this->app->requireJS($this->assets_path . $js);
+        }
+
+        foreach ($this->required_css as $css) {
+
+            if($this->isAssetLoaded($css)) {
+                continue;
+            }
+
+            self::$loaded_assets[] = $css;
+
+            $this->app->requireCSS($this->assets_path . $css);
+        }
+    }
+
+    private function isAssetLoaded($asset) :bool
+    {
+        return in_array($asset,self::$loaded_assets, false);
+    }
+
+    private function addRequiredPlugin($plugin_asset) :void
+    {
+
+        $plugin_asset = $this->assets_path . $plugin_asset;
+
+        if($this->isAssetLoaded($plugin_asset)) {
+            return;
+        }
+
+        self::$loaded_assets[] = $plugin_asset;
+
+        $this->app->requireJS($plugin_asset);
+    }
+}
